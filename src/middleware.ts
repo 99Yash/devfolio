ee78a80dcc846848fef996e2355b837d1aab1831
yash <yashgouravkar@gmail.com>
2023-06-17 09:18:37 +0530

    fix: fixed clerk public route issue

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,15 @@
 import { authMiddleware } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
 export default authMiddleware({
-  publicRoutes: ['/', '/portfolio/:userId'],
+  signInUrl: '/sign-in',
+  publicRoutes: ['/', '/portfolio/(.*)'],
+  async afterAuth(auth, req) {
+    if (auth.isPublicRoute) {
+      //Dont do anything
+      return NextResponse.next();
+    }
+  },
 });
 
 export const config = {
